2186390a0efba4d71df78a76c77ba940710c59ed
Seungwon <jshtmddnjs@naver.com>
2020-11-26 15:57:29 +0900

    Question Model Demo class

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 # In this App I learned
 
 * include snippet code for reuse case
+* startapp & setting.py
+* makemigrate -> notifying django migration, migrate -> commit migration
+* for loop using Django
+* class Meta(verbose_name, verbose_name_plural)
--- personal/views.py
+++ personal/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
+from personal.models import Question
 
 # Create your views here.
 
 def home_screen_view(request):
-	print(request.headers)
 
 	# context = {}
 	# context['some_string'] = "this is some string that I'm passing to the view"
@@ -14,12 +14,15 @@ def home_screen_view(request):
 	# 	'some_number': 123124
 	# }
 
-	list_of_values = []
-	list_of_values.append('first entry')
-	list_of_values.append('second entry')
-	list_of_values.append('third entry')
-	list_of_values.append('fourth entry')
-	context = {}
-	context['list_of_values'] = list_of_values
+	# list_of_values = []
+	# list_of_values.append('first entry')
+	# list_of_values.append('second entry')
+	# list_of_values.append('third entry')
+	# list_of_values.append('fourth entry')
+	# context = {}
+	# context['list_of_values'] = list_of_values
 
+	questions = Question.objects.all()
+	context = {}
+	context['questions'] = questions
 	return render(request, "personal/home.html", context)
