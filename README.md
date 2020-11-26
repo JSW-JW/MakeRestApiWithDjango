868c239826087da20655c4e4a7bd1069b74e6aad
Seungwon <jshtmddnjs@naver.com>
2020-11-26 11:48:17 +0900

    passing data from views.py to view to snippet view

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# This repo is for practicing Django basic stuff and in sequence, for making a RestApi with python.
+
+# Description
+
+* This repo is for practicing Django basic stuff and in sequence, for making a RestApi with python.
+
+# In this App I learned
+
+* include snippet code for reuse case
--- personal/views.py
+++ personal/views.py
@@ -4,4 +4,22 @@ from django.shortcuts import render
 
 def home_screen_view(request):
 	print(request.headers)
-	return render(request, "personal/home.html", {})
+
+	# context = {}
+	# context['some_string'] = "this is some string that I'm passing to the view"
+	# context['some_number'] = 123124
+
+	# context = {
+	# 	'some_string': "this is some string that I'm passing to the view",
+	# 	'some_number': 123124
+	# }
+
+	list_of_values = []
+	list_of_values.append('first entry')
+	list_of_values.append('second entry')
+	list_of_values.append('third entry')
+	list_of_values.append('fourth entry')
+	context = {}
+	context['list_of_values'] = list_of_values
+
+	return render(request, "personal/home.html", context)
