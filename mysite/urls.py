469eb7a9bf6801167a78b84aafcd6c449b572f66
Seungwon <jshtmddnjs@naver.com>
2020-12-05 16:55:11 +0900

    Form & Validation

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,17 +17,19 @@ from django.contrib import admin
 from django.urls import path
 
 from personal.views import (
-	home_screen_view,
+    home_screen_view,
 )
 
 from account.views import (
     registration_view,
     logout_view,
+    login_view,
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home_screen_view, name="home"),
-    path('register/', registration_view, name="register"),
-    path('logout/', logout_view, name="logout")
+    path("admin/", admin.site.urls),
+    path("", home_screen_view, name="home"),
+    path("register/", registration_view, name="register"),
+    path("logout/", logout_view, name="logout"),
+    path("login/", login_view, name="login"),
 ]
